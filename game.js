557cc8cf83b08271fcd414b94750bfd2f200cb92
zin-wai <lwinzinwai31@gmail.com>
2023-03-17 10:49:05 +0630

    display,correct question and answer

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 const question = document.getElementById("question");
-const chioces = Array.from(document.getElementsByClassName("chioce-text"));
+const choices = Array.from(document.getElementsByClassName("chioce-text"));
 // console.log(chioces);
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -43,8 +43,60 @@ const MAX_QUESTIONS = 3;
 startGame = () => {
     questionCounter = 0;
     score = 0;
-    availableQuestions= [...questions];
-    console.log(availableQuestions);
+    availableQuestions = [...questions];
+    // console.log(availableQuestions);
+    getNewQuestion();
 };
+
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS){
+        return window.location.assign('./end.html');
+    }
+
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach(choice => {
+        const number = choice.dataset["number"];
+        choice.innerText = currentQuestion["choice" + number];
+    });
+
+    availableQuestions.splice(questionIndex , 1);
+
+    acceptingAnswers = true;
+};
+
+choices.forEach(chioce => {
+    chioce.addEventListener("click", e => {
+        // console.log(e.target);
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+        
+        console.log(selectedAnswer == currentQuestion.answer);
+
+        // Method 1
+        // const classToApply = "incorrect";
+        // if(selectedAnswer == currentQuestion.answer){
+        //     classToApply = "correct";
+        // }
+
+        // Method 2
+        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+        console.log(classToApply);
+
+        selectedChoice.parentElement.classList.add(classToApply);
+
+        setTimeout( () => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        },1000);        
+    });
+});
+
 
 startGame();
